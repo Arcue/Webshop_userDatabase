37720ea144a23f738be6b3fbdd792d2be1bcb528
JolleJ <joeljansson96@gmail.com>
2018-10-11 11:08:47 +0300

    Fixing agagin

--- UserAPI/UserAPI/Startup.cs
+++ UserAPI/UserAPI/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using UserAPI.Models;
 
 namespace UserAPI
 {
@@ -24,7 +26,20 @@ namespace UserAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            
+            var connection = "Database=dfug8uq2aj17f1; host=ec2-54-246-101-215.eu-west-1.compute.amazonaws.com; port=5432; Username=qnuavllldruxiq; password=3a39919f5963f461db1eba8957ebb1b00293e12e77aea0ffc864114f6404f8ee;SslMode=Prefer;TrustServerCertificate=True;";
+  
+            services.AddDbContext<dfug8uq2aj17f1Context>(options => options.UseSqlServer(connection));
+            //services.AddDbContext
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +49,7 @@ namespace UserAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            
 
             app.UseMvc();
         }
